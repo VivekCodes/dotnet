e49dfa59760e2c653c2b6e7f2499dc0b7adc3798
Vivek Kumar <vkkiit@gmail.com>
2020-02-29 22:01:34 +0530

    First Commit.
    First Commit.

---
+++ CallerMethodInformation/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    trying example from the guide below, with minor changes:
+    https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/caller-information
+*/
+
+
+using System;
+
+namespace CallerMethodInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.CallPrintCallerInformation();
+        }
+
+        public void CallPrintCallerInformation()
+        {
+            PrintCallerInformation("Message To Print.");
+        }
+        public void PrintCallerInformation(string message,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {            
+            Console.WriteLine("Message: " + message);
+            Console.WriteLine("Member Name: " + memberName);
+            Console.WriteLine("Source File Path: " + sourceFilePath);
+            Console.WriteLine("Source Line Number: " + sourceLineNumber);
+        }
+    }
+}
